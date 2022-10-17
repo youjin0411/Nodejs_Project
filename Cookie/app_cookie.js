f7d0e3aafb470bd6a661639df628a4db94ba920f
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-10-17 23:31:16 +0900

    count 완성

--- Cookie/app_cookie.js
+++ Cookie/app_cookie.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser()); //cookieParser이 우리 app에 사용됨
 app.get('/count', function(req, res){
-    res.send('count : ');
+    //웹브라우저에게 응답할 때 count를 보낸다. response (res)는 응답 
+    if(req.cookies.count){
+        //count의 값이 있다면 count변수에 넣고
+        var count = parseInt(req.cookies.count); //강제형 정수 변환 -> 값이 문자 1일 시 숫자 1이됨 
+    }else{
+        //없다면 0으로 초기화
+        var count = 0;
+    }
+    count = count + 1;
+    res.cookie('count', count); //count의 값으로 1을 줌
+    res.send('count : ' + count);
 });
 app.listen(8080, function(){
     console.log('Connected 8080 port !!!');
